406f3eb75a9e87c56e3103f74de1c3d55be9f701
Samiullah <samiullahtufail181@gmail.com>
2024-04-23 12:56:23 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let answers = await inquirer.prompt([
+  {
+    name: "sentence",
+    message: "enter you sentence",
+    type: "input",
+  },
+]);
+
+let sentence_Length = answers.sentence.split(" ").filter((element: any) => {
+  return element.length !== 0;
+});
+
+console.log("the number of words is :", sentence_Length.length);
